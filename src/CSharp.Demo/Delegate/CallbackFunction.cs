613023d2ad7480f6b7ef3a10c69f0bd98c3b16c9
SongWenJie <719172036@qq.com>
2018-08-21 16:51:46 +0800

    Delegate

---
+++ src/CSharp.Demo/Delegate/CallbackFunction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class CallbackFunction
+    {
+
+        public static void Double(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] * 2;
+            }
+        }
+
+        public static void AddOne(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + 1;
+            }
+        }
+
+        public static void DoubleAndAddOne(int[] nums,Func<int,int> func)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = func(nums[i] * 2);
+            }
+        }
+    }
+}
---
+++ src/CSharp.Demo/Delegate/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate(1, 2, Add);
+            Calculate(1, 1, Divisi);
+
+            Calculate(1, 2, delegate (double a,double b) { return a + b; });
+            Calculate(1, 1, delegate (double a, double b) { return a / b; });
+
+            Calculate(1, 2, (double a,double b) => { return a + b; });
+            Calculate(1, 2, (a,b) => { return a + b; });
+
+
+            int[] nums = { 1, 2, 3 };
+
+            //CallbackFunction.Double(nums);
+            //CallbackFunction.AddOne(nums);
+
+            CallbackFunction.DoubleAndAddOne(nums, n => n + 1);
+        }
+
+        public enum Operate
+        {
+            Add,
+            Subtrac,
+            Multip,
+            Divisi
+        }
+
+        public delegate double CalculateDelegate(double a, double b);
+
+        public static double Calculate(double a, double b, CalculateDelegate operate)
+        {
+            return operate(a, b);
+        }
+
+
+        public static double Calculate(double a,double b, Operate operate)
+        {
+            switch(operate)
+            {
+                case Operate.Add:
+                    return Add(a, b);
+                case Operate.Subtrac:
+                    return Subtrac(a, b);
+                case Operate.Multip:
+                    return Multip(a, b);
+                case Operate.Divisi:
+                    return Divisi(a, b);
+                default:
+                    return 0;
+            }
+        }
+
+        public static double Add(double a , double b)
+        {
+            return a + b;
+        }
+        public static double Subtrac(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multip(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divisi(double a, double b)
+        {
+            if (b == 0) throw new Exception("分母不能为0");
+            return a / b;
+        }
+
+
+
+    }
+}
